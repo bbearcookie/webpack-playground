c0af7a9284336e0c458bf9f5bedbf8b1b21d23ba
bbearcookie <aodem@naver.com>
2024-01-06 01:05:27 +0900

    feat: main.tsx 및 index.html 작성

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <div>어서오세요! App이에요</div>
+    </div>
+  );
+};
+
+export default App;
--- src/Counter.tsx
+++
@@ -1,7 +0,0 @@
-const Counter = () => {
-  return (
-    <div>
-      <div>아직은 가짜 카운터에요</div>
-    </div>
-  );
-};
